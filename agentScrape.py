37f42e35559fdaff4a9c737bece4c4fda5ea40c4
Jonathan Rubin <jonathan.a.rubin@gmail.com>
2017-08-13 11:40:06 -0400

    Refactored to use functions and updated code to be in line with pep8

--- agentScrape.py
+++ agentScrape.py
@@ -1,54 +1,61 @@
-import openpyxl as xl
-import webbrowser
 import requests
-import bs4
-import time
-import pandas as pd
-
-
-#currently testing Ohio
-state2 = 'oh'
-
-#currently manually entering list of cities
-cities = ['akron','canton']#,'cincinnati']#,'cleveland','columbus','toledo','dayton','findlay','toledo','youngstown']
-
-# Blank list used to create list and list of lists, which will eventually be put into dataframe
-allAgentContactInfo = []
-agentContactInfo = []
-
-#loop thru cities in list
-for city in cities:
-	siteWithCity = 'http://agency.nationwide.com/' + city + '-oh?page='
-
-	#loop thru pages of each city
-	for page in range(1,10):
-		citiesRequest = requests.get(siteWithCity + str(page))
-		citiesBs = bs4.BeautifulSoup(citiesRequest.content, 'html.parser')
-		officeNameList = list(citiesBs.find_all('span', itemprop="name"))
-		officeNameList2 = citiesBs.find_all('span', itemprop="name")
-		officeTelephone = citiesBs.find_all('strong', itemprop="telephone")
-		streetAddressList = citiesBs.find_all('div', itemprop="streetAddress")
-		addressLocalityList = citiesBs.find_all('span', itemprop="addressLocality")
-		addressRegionList = citiesBs.find_all('span', itemprop="addressRegion")
-		postalCodeList = citiesBs.find_all('span', itemprop="postalCode")
-		time.sleep(4)
-
-
-		for name, name2, telephone, address, locality, state, zipCode in zip(officeNameList2[::2], officeNameList2[1::2], officeTelephone, streetAddressList, addressLocalityList, addressRegionList, postalCodeList):
-			#had to adjust the tags and find way to remove \n's
-			agentContactInfo.append(str(name.getText()).replace('\n',''))
-			
-			#adding office name
-			agentContactInfo.append(name2.getText())
-			agentContactInfo.append(telephone.getText())
-			agentContactInfo.append(address.getText())
-			agentContactInfo.append(locality.getText())
-			agentContactInfo.append(state.getText())
-			agentContactInfo.append(zipCode.getText())
-			
-			#append each agent's info to the list of lists and then clear for next agent
-			allAgentContactInfo.append(agentContactInfo)
-			agentContactInfo = []
-
-print(allAgentContactInfo)
-pd.DataFrame(allAgentContactInfo).to_csv('scrapeOh11.csv')
+from bs4 import BeautifulSoup
+import sys
+import csv
+ 
+cities = ['akron', 'canton', 'cincinnati', 'cleveland', 'columbus', 'dayton', 'kent', 'youngstown']
+urls_list = []
+ 
+# URL = "http://agency.nationwide.com/{0}-{1}".format(city, state)
+ 
+def get_list_of_urls(state):
+ 
+    for city in cities:
+        url = "http://agency.nationwide.com/{0}-{1}?".format(city, state)
+        for page in range(1, 8):
+            url_with_page = (url + "page=" + str(page))
+            urls_list.append(url_with_page)
+    return urls_list
+ 
+def scrape_agency_info(urls_list):
+    """Returns lists of lists for agent info for a single page"""
+    agent_info_list = []
+    indiv_agent_info_list = []
+    page_of_agent_info = []
+ 
+    for url in urls_list:
+        # get html
+        nationwide_request = requests.get(url)
+        nationwide_bs = BeautifulSoup(nationwide_request.content, 'html.parser')
+ 
+        # loop thru each office on page, remove html/css, and place item in list with all similar items from other offices
+        office_name_list = [x.getText() for x in nationwide_bs.find_all("span", itemprop="name")]
+        office_telephone_list = [x.getText() for x in nationwide_bs.find_all("strong", itemprop="telephone")]
+        office_address_list = [x.getText() for x in nationwide_bs.find_all("div", itemprop="streetAddress")]
+        office_city_list = [x.getText() for x in nationwide_bs.find_all("span", itemprop="addressLocality")]
+        office_state_list = [x.getText() for x in nationwide_bs.find_all("span", itemprop="addressRegion")]
+        office_zip_code_list = [x.getText() for x in nationwide_bs.find_all("span", itemprop="postalCode")]
+     
+        # loop thru each list of items and append to list for each individual office to match up with other info
+        for name, telephone, address, city, state, zip_code in zip(office_name_list[1::2], office_telephone_list, office_address_list, office_city_list, office_state_list, office_zip_code_list):
+            page_of_agent_info.append([name, telephone, address, city, state, zip_code])
+            agent_info_list.append(page_of_agent_info)
+            page_of_agent_info = []
+ 
+    return agent_info_list
+ 
+def write_offices_to_csv(agencies):
+    """Write *agent_info_list* to a csv file called 'football_players.csv'"""
+    with open("/Users/path/refactoredScrape.csv", "w") as nationwide_csv:
+        writer = csv.writer(nationwide_csv)
+        for each_office in agencies:
+            writer.writerow(each_office)
+ 
+def main():
+    """Main entry point for script"""
+    get_list_of_urls("oh")
+    agent_info_list = scrape_agency_info(urls_list)
+    write_offices_to_csv(agent_info_list)
+ 
+if __name__ == '__main__':
+    sys.exit(main())
